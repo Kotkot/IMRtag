e8df5da553e9759d355ac2f16dac6083caa52368
holleland <sondre@skalvi.se>
2021-12-17 08:02:51 +0100

    adding comments

--- R/adding_points_to_kotaros_maps.R
+++ R/adding_points_to_kotaros_maps.R
@@ -13,14 +13,14 @@ d <- filter(d,
             !(ices_rectangle %in% unique(d$ices_rectangle)[unlist(sapply("F", function(x) grep(x, unique(d$ices_rectangle))))]))
 
 
+# Releases:
 df_rel <- d %>% filter(between(year(releasedate),2014,2020))
-df_rel$longitude
 df_rel <- mutate(df_rel,
                  iceslon =mapplots::ices.rect(ices_rectangle)$lon,
                  iceslat = mapplots::ices.rect(ices_rectangle)$lat)
 
 
-
+# Recaptures:
 recap_raw <- df_rel %>% filter(!is.na(recapturedate)) %>%
   left_join(Catch_data, by = "catchid") %>%
   mutate(RecaptureYear = ifelse(month(recapturedate)>2,
@@ -31,22 +31,22 @@ recap_raw <- df_rel %>% filter(!is.na(recapturedate)) %>%
   filter(YearsOut<3, between(ReleaseYear,2014,2020),
          !is.na(ices_rectangle.y))
 
+# Get map:
 xlim = range(c(recap_raw$clon,df_rel$longitude) , na.rm = T)
 ylim = range(c(recap_raw$clat,df_rel$latitude), na.rm = T)
-
-
-
-# Get map:
 m <- ggplot2::map_data("world",
                        xlim = xlim + c(-.4,1),
                        ylim = ylim +c(-.1,.1) )
 
+# Simple plot:
 ggplot()+
   geom_polygon(data = m, aes(long, lat, group = group), fill = "grey") +
   coord_quickmap(xlim = xlim + c(-.5,.5), ylim = ylim +c(0,0)) +
   theme_bw()+
+  # raw releases:
   geom_point(data = df_rel %>% distinct(longitude,latitude),    aes(x = longitude, y = latitude), col = "red", size = .1)+
-  geom_point(data = df_rel %>% distinct(iceslon,iceslat),    aes(x = iceslon, y = iceslat), col = "red", size = .1)+
+  # ices rectangles:
+  #geom_point(data = df_rel %>% distinct(iceslon,iceslat),    aes(x = iceslon, y = iceslat), col = "red", size = .1)+
   geom_point(data = recap_raw %>% distinct(clon,clat), aes(x = clon, y = clat), col = "#FEE724", size = .1)+
   theme(panel.background = element_rect(fill = "#00008B"),
         panel.grid = element_blank())
